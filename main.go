5535a49dcf91fab5c29a6faf8c0d3a79ca5fc349
tickstep <tickstep@outlook.com>
2020-07-30 22:29:50 +0800

    update version to v0.0.2

--- main.go
+++ main.go
@@ -32,7 +32,7 @@ const (
 
 var (
 	// Version 版本号
-	Version = "v0.0.1"
+	Version = "v0.0.2"
 
 	historyFilePath = filepath.Join(config.GetConfigDir(), "cloud189_command_history.txt")
 	reloadFn        = func(c *cli.Context) error {
